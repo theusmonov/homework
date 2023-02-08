56b84fc01fc253c8a691b7123e9d201a31a61d79
theusmonov <asadbek.usmonoff2003@gmail.com>
2023-02-08 15:26:12 +0500

    js to'liq vazifa

--- main.js
+++ main.js
@@ -1,3 +1,321 @@
+// 1-masala
+
+// function karraSon(k,n){
+//     for(  let i = 0; i < n; i++){
+//         console.log(k);
+//     }
+// }
+// karraSon(2,10)
+
+// 2-masala
+
+// function harfLar(a,b){
+//     for(let i=a; i > b -1; i--){
+//         console.log(i);
+//     }
+// }
+// harfLar(40,20)
+
+
+// 3-masala
+
+// function harfLar2(a,b){
+//         for(let i=a -1; i > b ; i--){
+//             console.log(i);
+//         }
+//     }
+//     harfLar2(40,20)
+
+
+
+
+// // 4-masala
+// let chocalate=[1,2,3,4,5,6,7,8,9,10];
+// function moneyChocolate(chocalate){
+//     for(let i= 0; i < chocalate.length; i++){
+//         let price =10000;
+//         console.log(price *= chocalate[i])
+//     }
+// }
+// moneyChocolate(chocalate)
+
+
+
+
+// 5/6-masala
+
+// let kg = prompt('sonni kiriting')
+// kg =Number(kg)
+
+// function olma(a){
+//     let count = 10000
+//     return a * count
+// }
+// let resalt = olma(kg)
+// console.log(resalt);
+
+
+
+// 7-masala
+
+// function sonlar(num){
+//     let a =0
+//     for(let i= 0; i<=num; i++){
+//       a += i
+//     }
+//     return a
+// }
+// console.log(sonlar(num));
+
+
+// 8-masala
+// function sonlar(num){
+//     let a=1
+//     for(let i= 1; i<=num; i++){
+//       a *= i
+//     }
+//     return a
+// }
+// console.log(sonlar(num));
+
+// 9-masala
+
+
+
+// 11-masala
+
+// let n =100;
+
+// for(let i=1; i<= n; i++){
+//     if(i%2 !== 0){
+//         console.log(i);
+//     }
+// }
+
+
+// 12-masala
+// let n =100;
+// for(let i=1; i<= n; i++){
+//     if(i%2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+// 15-masala
+
+// function raqamlar(n){
+//     for(let i=n; i > 0; i--){
+//        console.log(i); 
+//     }
+// } 
+// raqamlar(100)
+
+
+// 16-masala
+
+// let n =100;
+// for(let i=1; i<= n; i++){
+//     if( i%5 !==0 ){
+//         console.log(i);
+//     }
+// }
+
+// 17-masala
+
+
+// let num = prompt('sonni kiriting')
+// num =Number(num)
+
+// function calc(num){
+//     let count =1
+//     for(let i=1; i<=num; i++){
+//       count *= i
+//     } return count
+// }
+// console.log(calc(num));
+
+
+
+
+
+
+
+
+
+
+
+// switch case
+
+// 1-masala
+
+// let weekday = 7;
+
+// switch (weekday){
+//     case 1:
+//         console.log('dushanba');
+//     break;
+//     case 2:
+//         console.log('seshanba');
+//     break;
+//     case 3:
+//         console.log('chorshanba');
+//     break;
+//     case 4:
+//         console.log('payshanba');
+//     break;
+//     case 5:
+//         console.log('juma');
+//     break;
+//     case 6:
+//         console.log('shanba');
+//     break;
+//     case 7:
+//         console.log('yakshanba');
+//     break;
+
+//     default: 
+//     console.log('no this day ');
+// }
+
+
+// 2-masala
+
+// let baho = 5;
+
+// switch(baho){
+//     case 1:
+//         console.log('yomon');
+//     break;
+//     case 2:
+//         console.log('qoniqarsiz');
+//     break;
+//     case 3:
+//         console.log('qoniqarli');
+//     break;
+//     case 4:
+//         console.log('yaxshi');
+//     break;
+//     case 5:
+//         console.log('alo');
+//     break;
+//     default:
+//         console.log('bunday baho mavjud emas');
+
+// }
+ 
+// 3-masala
+
+// let oy = 12;
+// switch(oy){
+//     case 12:
+//         console.log('qish');
+//     break;  
+    
+//     case 1:
+//         console.log('qish');
+//     break;  
+//     case 2:
+//         console.log('qish');
+//     break;  
+//     case 3:
+//         console.log('bahor');
+//     break; 
+//     case 4:
+//         console.log('bahor');
+//     break; 
+//     case 5:
+//         console.log('bahor');
+//     break; 
+//     case 6:
+//         console.log('yoz');
+//     break; 
+//     case 7:
+//         console.log('yoz');
+//     break; 
+//     case 8:
+//         console.log('yoz');
+//     break; 
+//     case 9:
+//         console.log('kuz');
+//     break; 
+//     case 10:
+//         console.log('kuz');
+//     break; 
+//     case 11:
+//         console.log('kuz');
+//     break; 
+//     default:
+//         console.log('no this month'); 
+// }  
+
+
+// 4-masala
+
+// let oy = 10;
+
+// switch(oy){
+//     case 1:
+//         console.log('31 kun');
+//     break; 
+//     case 2:
+//         console.log('28 kun');
+//     break;    
+//     case 3:
+//         console.log('31 kun');
+//     break; 
+//     case 4:
+//         console.log('30 kun');
+//     break; 
+//     case 5:
+//         console.log('31 kun');
+//     break; 
+//     case 6:
+//         console.log('30 kun');
+//     break; 
+//     case 7:
+//         console.log('31 kun');
+//     break; 
+//     case 8:
+//         console.log('31 kun');
+//     break; 
+//     case 9:
+//         console.log('30 kun');
+//     break; 
+//     case 10:
+//         console.log('31 kun');
+//     break; 
+//     case 11:
+//         console.log('30 kun');
+//     break; 
+//     case 12:
+//         console.log('31 kun');
+//     break; 
+// }
+
+
+
+// 6-masala
+
+// let stipendiya = 4;
+// switch(stipendiya){
+//     case 3:
+//         console.log('317 ming');
+//     break;  
+//     case 4:
+//         console.log('417 ming');
+//     break;    
+//     case 5:
+//         console.log('517 ming');
+//     break;  
+
+//     default:  
+//         console.log('2 bahoga stipendiya yuq');
+// }
+ 
 
 // turnari aperatrs
 
@@ -47,7 +365,7 @@
 // let res = yil - yosh 
 
 
-// let a = 15 ;
+// let a = 22 ;
 // let b = 20 ;
 // let c = 30 ;
 // let natija = a <= b <= c && a <= c && a <= b ? 'Natija togri' : 'Natija xato'
